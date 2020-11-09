eab7639df9274d73cf07af026ab3ddf349ab0410
trongthanh01 <19021367@vnu.edu.vn>
2020-11-09 20:09:00 +0700

    starter

--- src/uet/oop/bomberman/entities/Entity.java
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -1,31 +1,65 @@
 package uet.oop.bomberman.entities;
 
-import javafx.scene.SnapshotParameters;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import uet.oop.bomberman.graphics.IRender;
+import uet.oop.bomberman.graphics.Screen;
 import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.level.Coordinates;
 
-public abstract class Entity {
-    protected int x;
-    protected int y;
-    protected Image img;
+/**
+ * Lớp đại diện cho tất cả thực thể trong game (Bomber, Enemy, Wall, Brick,...)
+ */
+public abstract class Entity implements IRender {
 
-    public Entity( int x, int y, Image img) {
-        this.x = x;
-        this.y = y;
-        this.img = img;
-    }
+	protected double _x, _y;
+	protected boolean _removed = false;
+	protected Sprite _sprite;
 
-    public void render(GraphicsContext gc) {
-        SnapshotParameters params = new SnapshotParameters();
-        params.setFill(Color.TRANSPARENT);
+	/**
+	 * Phương thức này được gọi liên tục trong vòng lặp game,
+	 * mục đích để xử lý sự kiện và cập nhật trạng thái Entity
+	 */
+	@Override
+	public abstract void update();
 
-        ImageView iv = new ImageView(img);
-        Image base = iv.snapshot(params, null);
+	/**
+	 * Phương thức này được gọi liên tục trong vòng lặp game,
+	 * mục đích để cập nhật hình ảnh của entity theo trạng thái
+	 */
+	@Override
+	public abstract void render(Screen screen);
+	
+	public void remove() {
+		_removed = true;
+	}
+	
+	public boolean isRemoved() {
+		return _removed;
+	}
+	
+	public Sprite getSprite() {
+		return _sprite;
+	}
 
-        gc.drawImage(base, x * Sprite.SCALED_SIZE, y * Sprite.SCALED_SIZE);
-    }
-    public abstract void update();
+	/**
+	 * Phương thức này được gọi để xử lý khi hai entity va chạm vào nhau
+	 * @param e
+	 * @return
+	 */
+	public abstract boolean collide(Entity e);
+	
+	public double getX() {
+		return _x;
+	}
+	
+	public double getY() {
+		return _y;
+	}
+	
+	public int getXTile() {
+		return Coordinates.pixelToTile(_x + _sprite.SIZE / 2);
+	}
+	
+	public int getYTile() {
+		return Coordinates.pixelToTile(_y - _sprite.SIZE / 2);
+	}
 }
