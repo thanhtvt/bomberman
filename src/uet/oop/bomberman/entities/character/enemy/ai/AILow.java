a9f7cdd18b26e1bfe0cbc90733a19b0a689a54c4
ilovengochuyen <61539998+ilovengochuyen@users.noreply.github.com>
2020-11-29 19:04:55 +0700

    testrandom

--- src/uet/oop/bomberman/entities/character/enemy/ai/AILow.java
+++ src/uet/oop/bomberman/entities/character/enemy/ai/AILow.java
@@ -1,11 +1,14 @@
 package uet.oop.bomberman.entities.character.enemy.ai;
+import java.util.Random;
 
 public class AILow extends AI {
 
 	@Override
 	public int calculateDirection() {
 		// TODO: cài đặt thuật toán tìm đường đi
-		return 1;
+		Random generator = new Random();
+		int value = generator.nextInt(5) + 1;
+		return value;
 	}
 
 }
